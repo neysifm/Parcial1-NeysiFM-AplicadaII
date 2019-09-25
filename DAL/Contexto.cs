94f5baf85649aed440b5377193ffae0be1545e17
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-09-24 20:57:04 -0400

    Completando Estructura del Primer Parcial, Aplicada II.

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
+        //public DbSet< >  { get; set; }
+
+        public Contexto() : base("Constr")
+        {
+        }
     }
 }
